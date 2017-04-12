11770e7e3492bd49f88bb3904bf2803eba485cc1
Andreas Grosam <agrosam@onlinehome.de>
2017-04-12 20:00:14 +0200

    Initial Commit

---
+++ TaskQueue/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TaskQueue
+//
+//  Created by Andreas Grosam on 12.04.17.
+//  Copyright © 2017 Andreas Grosam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
