36a22c68b60e122521f3d875c994586e6710b594
Andreas Grosam <agrosam@onlinehome.de>
2017-04-14 21:29:52 +0200

    Initial Commit

---
+++ TaskQueue/TaskQueue.swift
@@ -0,0 +1,153 @@
+//
+//  TaskQueue.swift
+//
+//  Copyright © 2017 Andreas Grosam. All rights reserved.
+//
+
+import Dispatch
+
+
+/**
+ A `TaskQueue` is a FIFO queue where _tasks_ can be enqueued for execution. The 
+ tasks will be executed in order up to `maxConcurrentTasks` concurrently. The start 
+ of a task will be delayed up until the current number of running tasks is below 
+ the allowed maximum number of concurrent tasks.
+ 
+ When a task completes it will be automatically dequeued and its completion handler
+ will be called.
+ 
+ The allowed maximum number of concurrent tasks can be changed while tasks are 
+ executing.
+`
+ A _task_ is simply a non-throwing asynchronous function with a single parameter
+ `completion` which is a function type. The completion function has a single parameter 
+ which desginates the result type of the task. On completion, the task MUST call 
+ the completion handler passing the result. Usually, the result type is a discriminated 
+ union (aka Enum) which contains either a value or an error.
+*/
+public class TaskQueue {
+
+    private let _queue: DispatchQueue
+    private var _maxConcurrentTasks: UInt = 1
+    private var _concurrentTasks: UInt = 0
+    private let _group = DispatchGroup()
+    private let _syncQueue = DispatchQueue(label: "task_queue.sync_queue")
+
+
+    /// Designated initializer.
+    ///
+    /// - Parameter maxConcurrentTasks: The number of tasks which can be executed concurrently.
+    public init(maxConcurrentTasks: UInt = 1) {
+        self._queue = DispatchQueue(label: "task_queue.queue", target: _syncQueue)
+        _maxConcurrentTasks = maxConcurrentTasks
+    }
+
+
+    /// Enqueues the given task and returns immediately.
+    ///
+    /// The task will be executed when the current number of active tasks is 
+    /// smaller than `maxConcurrentTasks`.
+    /// - Parameters:
+    ///   - task: The task which will be enqueued.
+    ///   - queue: The dispatch queue where the task should be started.
+    ///   - completion: The completion handler which will be executed when the task completes. It will be executed on whatever execution context the task has been choosen.
+    /// - Parameters:
+    ///   - result: The task's result.
+    /// - Attention: There's no upper limit for the number of enqueued tasks. An enqueued task may reference resources and other objects which will be only released when the task has been completed.
+    public final func enqueue<T>(task: @escaping (@escaping (T)->())->(), queue q: DispatchQueue = DispatchQueue.global(), completion: @escaping (_ result: T)->()) {
+        //print("enqueue task")
+        self._queue.async {
+            //print("start task")
+            self.execute(task: { c in q.async {task(c)} } ) { result in
+                //print("complete task")
+                completion(result)
+            }
+        }
+    }
+
+
+    /// Executes the given task and returns immediately.
+    ///
+    /// - Parameters:
+    ///   - task: The task which will be enqueued.
+    ///   - queue: The dispatch queue where the task should be started.
+    ///   - completion: The completion handler that will be executed when the task completes.
+    private final func execute<T>(task: @escaping (@escaping (T)->())->(), completion: @escaping (T)->()) {
+        assert(_concurrentTasks < _maxConcurrentTasks)
+        _concurrentTasks += 1
+        if _concurrentTasks == _maxConcurrentTasks {
+            self._queue.suspend()
+        }
+        self._group.enter()
+        task() { result in
+            self._syncQueue.async {
+                if self._concurrentTasks == self._maxConcurrentTasks {
+                    self._queue.resume()
+                }
+                self._concurrentTasks -= 1
+                self._group.leave()
+            }
+            completion(result)
+        }
+    }
+
+
+    /// Enqueues the given function for barrier execution and returns immediately.
+    ///
+    /// A barrier function allows you to create a synchronization point within the
+    /// `TaskQueue`. When the `TaskQueue` encounters a barrier function, it delays
+    /// the execution of the barrier function and any further tasks until all tasks 
+    /// enqueued before the barrier finish executing. At that point, the barrier 
+    /// function executes exclusively. Upon completion, the `TaskQueue` resumes 
+    /// its normal execution behavior.
+    ///
+    /// - Parameters:
+    ///   - queue: The dispatch queue where the barrier should be executed.
+    ///   - f: The barrier function.
+    public final func enqueueBarrier(queue q: DispatchQueue = DispatchQueue.global(), f: @escaping ()->()) {
+        //print("enqueue barrier")
+        func barrier(completion: ()->()) {
+            //print("barrier wait for exclusive execution: number of running tasks: \(self._concurrentTasks - 1)")
+            self._queue.suspend()
+            self._group.notify(queue: q) {
+                //print("execute barrier func")
+                f()
+                self._syncQueue.async {
+                    //print("leave barrier")
+                    self._queue.resume()
+                }
+            }
+            completion()
+        }
+        self._queue.async {
+            //print("start barrier")
+            self.execute(task: barrier, completion: {})
+        }
+    }
+
+
+    /// Sets or returns the number of concurrently executing tasks.
+    public final var maxConcurrentTasks: UInt {
+        get {
+            var result: UInt = 0
+            _syncQueue.sync {
+                result = self._maxConcurrentTasks
+            }
+            return result
+        }
+        set (value) {
+            _syncQueue.async {
+                self._maxConcurrentTasks = value
+            }
+        }
+    }
+
+
+    public final var countRunningTasks: UInt {
+        return _syncQueue.sync {
+            return self._concurrentTasks
+        }
+    }
+
+
+}
---
+++ TaskQueueTests/TaskQueueTests.swift
@@ -0,0 +1,179 @@
+//
+//  TaskQueueTests.swift
+//  TaskQueueTests
+//
+//  Created by Andreas Grosam on 14.04.17.
+//  Copyright © 2017 Andreas Grosam. All rights reserved.
+//
+
+import XCTest
+import Dispatch
+@testable import TaskQueue
+
+
+func task(id: Int, delay: Double = 1.0) -> (_ completion: @escaping (Int) -> ()) -> () {
+    return { completion in
+        DispatchQueue.global().asyncAfter(deadline: .now() + delay) {
+            completion(id)
+        }
+    }
+}
+
+class TaskQueueTests: XCTestCase {
+
+    func testEnqueuedTaskWillComplete1() {
+        let taskQueue = TaskQueue()
+        let expect = expectation(description: "completed")
+        taskQueue.enqueue(task: task(id: 1)) {id in
+            XCTAssertEqual(1, id)
+            expect.fulfill()
+        }
+        waitForExpectations(timeout: 1)
+    }
+
+
+    func testEnqueuedTaskWillComplete2() {
+        let taskQueue = TaskQueue()
+        let expect1 = expectation(description: "completed 1")
+        let expect2 = expectation(description: "completed 2")
+        taskQueue.enqueue(task: task(id: 1)) { id in
+            XCTAssertEqual(1, id)
+            expect1.fulfill()
+        }
+        taskQueue.enqueue(task: task(id: 2)) { id in
+            XCTAssertEqual(2, id)
+            expect2.fulfill()
+        }
+        waitForExpectations(timeout: 1)
+    }
+
+
+    func ensureMaxConcurrentTasks(taskQueue: TaskQueue, numberTasks: Int, maxConcurrentTasks: UInt) {
+        let sem = DispatchSemaphore(value: Int(maxConcurrentTasks))
+        let syncQueue = DispatchQueue(label: "sync_queue")
+        var actualMaxConcurrentTasks: Int = 0
+
+        func task(id: Int) -> (_ completion: @escaping (Int) -> ()) -> () {
+            return { completion in
+                if case .timedOut = sem.wait(timeout: .now()) {
+                    XCTFail("semaphore overuse")
+                }
+                syncQueue.sync {
+                    actualMaxConcurrentTasks = max(actualMaxConcurrentTasks, Int(taskQueue.countRunningTasks))
+                }
+                DispatchQueue.global().asyncAfter(deadline: .now() + 0.2) {
+                    sem.signal()
+                    completion(id)
+                }
+            }
+        }
+
+        (1...numberTasks).forEach { id in
+            let expect = expectation(description: "completed")
+            taskQueue.enqueue(task: task(id: id), completion: { _ in
+                expect.fulfill()
+            })
+        }
+        waitForExpectations(timeout: 10)
+        XCTAssertEqual(maxConcurrentTasks, UInt(actualMaxConcurrentTasks))
+    }
+
+
+    func testMaxConcurrentTasks1() {
+        let maxConcurrentTasks: UInt = 1
+        let taskQueue = TaskQueue(maxConcurrentTasks: maxConcurrentTasks)
+        ensureMaxConcurrentTasks(taskQueue: taskQueue, numberTasks: 8, maxConcurrentTasks: maxConcurrentTasks)
+    }
+
+
+    func testMaxConcurrentTasks2() {
+        let maxConcurrentTasks: UInt = 2
+        let taskQueue = TaskQueue(maxConcurrentTasks: maxConcurrentTasks)
+        ensureMaxConcurrentTasks(taskQueue: taskQueue, numberTasks: 16, maxConcurrentTasks: maxConcurrentTasks)
+    }
+
+
+    func testMaxConcurrentTasks3() {
+        let maxConcurrentTasks: UInt = 3
+        let taskQueue = TaskQueue(maxConcurrentTasks: maxConcurrentTasks)
+        ensureMaxConcurrentTasks(taskQueue: taskQueue, numberTasks: 24, maxConcurrentTasks: maxConcurrentTasks)
+    }
+
+
+    func ensureBarrier(maxConcurrentTasks: UInt , before: Int, after: Int) {
+        let syncQueue = DispatchQueue(label: "snyc_queue")
+        var expectedSequence: [Int] = (0..<before).map { _ in 0} + [1] + (0..<after).map { _ in 2}
+        let expect = expectation(description: "barrier")
+        let taskQueue = TaskQueue(maxConcurrentTasks: maxConcurrentTasks)
+        func task(tag: Int) -> (_ completion: @escaping (Int) -> ()) -> () {
+            return { completion in
+                syncQueue.asyncAfter(deadline: .now() + 0.1) {
+                    guard let x = expectedSequence.first else {
+                        XCTFail("out of sequence")
+                        completion(-1)
+                        return
+                    }
+                    expectedSequence = Array(expectedSequence.dropFirst())
+                    XCTAssertEqual(tag, x)
+                    completion(x)
+                }
+            }
+        }
+
+        (1...before).forEach { id in
+            let expect = expectation(description: "completed")
+            taskQueue.enqueue(task: task(tag: 0), queue: syncQueue, completion: { _ in
+                expect.fulfill()
+            })
+        }
+        // When all "before" tasks finished, signal the sem:
+        taskQueue.enqueueBarrier(queue: syncQueue) {
+            guard let x = expectedSequence.first else {
+                XCTFail("out of sequence")
+                return
+            }
+            expectedSequence = Array(expectedSequence.dropFirst())
+            XCTAssertEqual(1, x)
+            sleep(1)
+            expect.fulfill()
+        }
+        (1...after).forEach { id in
+            let expect = expectation(description: "completed")
+            taskQueue.enqueue(task: task(tag: 2), completion: { _ in
+                expect.fulfill()
+            })
+        }
+        waitForExpectations(timeout: 10)
+    }
+
+
+    func testBarrier1() {
+        ensureBarrier(maxConcurrentTasks: 1, before: 4, after: 4)
+    }
+
+
+    func testBarrier2() {
+        ensureBarrier(maxConcurrentTasks: 2, before: 8, after: 8)
+    }
+
+
+    func testBarrier3() {
+        ensureBarrier(maxConcurrentTasks: 3, before: 12, after: 12)
+    }
+
+
+    func testBarrier4() {
+        ensureBarrier(maxConcurrentTasks: 4, before: 16, after: 16)
+    }
+
+
+    func testChangeMaxConcurrentTasks() {
+        let taskQueue = TaskQueue(maxConcurrentTasks: 1)
+        ensureMaxConcurrentTasks(taskQueue: taskQueue, numberTasks: 4, maxConcurrentTasks: 1)
+        taskQueue.enqueueBarrier {
+            taskQueue.maxConcurrentTasks = 8
+        }
+        ensureMaxConcurrentTasks(taskQueue: taskQueue, numberTasks: 32, maxConcurrentTasks: 8)
+    }
+
+}
