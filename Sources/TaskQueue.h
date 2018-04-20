5208522496ae478ec05352922e6d2bf76e0cc36c
Andreas Grosam <agrosam@onlinehome.de>
2018-04-20 12:26:51 +0200

    Add umbrella header

---
+++ Sources/TaskQueue.h
@@ -0,0 +1,15 @@
+//
+//  TaskQueue.h
+//  TaskQueue
+//
+//  Created by Andreas Grosam on 20.04.18.
+//  Copyright © 2018 Andreas Grosam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TaskQueue.
+FOUNDATION_EXPORT double TaskQueueVersionNumber;
+
+//! Project version string for TaskQueue.
+FOUNDATION_EXPORT const unsigned char TaskQueueVersionString[];
